976a5a0f4842a1dd9c3b8e4924c079129700463d
SFERAS <rthyssen73@gmail.com>
2023-06-19 15:14:30 -0300

    services 6 bloxes done

--- src/components/Routes/services/servicesBranches/ServicesBranches.jsx
+++ src/components/Routes/services/servicesBranches/ServicesBranches.jsx
@@ -14,45 +14,33 @@ function ServicesBranches() {
 			<div className="services__branches__coontainer">
 				<div className="services__branches__coontainer__boxes">
 					<img src={card1} />
-					<div className='text__boxes'>
-						<h>Software Development</h>
+						<h3>Software Development</h3>
 						<p>Transform your ideas into reality with our custom software development services. Our expert developers will create tailored solutions that align with your business goals, delivering high-quality code and seamless user experiences.						</p>
-					</div>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card2}/>
-					<div className='text__boxes'>
-						<h>Network Security</h>
+						<h3>Network Security</h3>
 						<p>Ensure the safety of your data and network infrastructure with our comprehensive network security services. We offer risk assessments, intrusion detection, firewall implementation, and ongoing monitoring to protect your valuable information from potential threats.						</p>
-					</div>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card3}/>
-					<div className='text__boxes'>
-						<h>Web Development</h>
+						<h3>Web Development</h3>
 						<p>Create a captivating web presence with our web development services. We design and develop visually appealing and functional websites that reflect your brand identity and drive conversions.						</p>
-					</div>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card4}/>
-					<div className='text__boxes'>
-						<h>Cloud Hosting</h>
+						<h3>Cloud Hosting</h3>
 						<p>Experience the benefits of flexible and reliable cloud hosting. Our experts will guide you through a seamless migration process, optimizing performance and reducing the burden of managing physical servers.						</p>
-					</div>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card5}/>
-					<div className='text__boxes'>
-						<h>Mobile Development</h>
+						<h3>Mobile Development</h3>
 						<p>Reach your target audience on their preferred devices with our mobile development services. We specialize in building intuitive and feature-rich mobile applications for both iOS and Android platforms.		</p>
-					</div>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img src={card6}/>
-					<div className='text__boxes'>
-						<h>UX/UI</h>
+						<h3>UX/UI</h3>
 						<p>Deliver exceptional user experiences with our UX/UI design services. Our designers combine aesthetics, usability, and functionality to create interfaces that engage users and drive customer satisfaction.		</p>
-					</div>
 				</div>
 
 
