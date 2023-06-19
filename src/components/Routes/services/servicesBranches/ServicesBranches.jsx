a71a32fdc367e85467b912f86980daec07607c27
SFERAS <rthyssen73@gmail.com>
2023-06-19 16:58:15 -0300

    includes added

--- src/components/Routes/services/servicesBranches/ServicesBranches.jsx
+++ src/components/Routes/services/servicesBranches/ServicesBranches.jsx
@@ -14,33 +14,33 @@ function ServicesBranches() {
 			<div className="services__branches__coontainer">
 				<div className="services__branches__coontainer__boxes">
 					<img src={card1} />
-						<h3>Software Development</h3>
-						<p>Transform your ideas into reality with our custom software development services. Our expert developers will create tailored solutions that align with your business goals, delivering high-quality code and seamless user experiences.						</p>
+						<h3 className='title__include'>Software Development</h3>
+						<p className='text__include'>Transform your ideas into reality with our custom software development services. Our expert developers will create tailored solutions that align with your business goals, delivering high-quality code and seamless user experiences.						</p>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card2}/>
-						<h3>Network Security</h3>
-						<p>Ensure the safety of your data and network infrastructure with our comprehensive network security services. We offer risk assessments, intrusion detection, firewall implementation, and ongoing monitoring to protect your valuable information from potential threats.						</p>
+						<h3 className='title__include'>Network Security</h3>
+						<p className='text__include'>Ensure the safety of your data and network infrastructure with our comprehensive network security services. We offer risk assessments, intrusion detection, firewall implementation, and ongoing monitoring to protect your valuable information from potential threats.						</p>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card3}/>
-						<h3>Web Development</h3>
-						<p>Create a captivating web presence with our web development services. We design and develop visually appealing and functional websites that reflect your brand identity and drive conversions.						</p>
+						<h3 className='title__include'>Web Development</h3>
+						<p className='text__include'>Create a captivating web presence with our web development services. We design and develop visually appealing and functional websites that reflect your brand identity and drive conversions.						</p>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card4}/>
-						<h3>Cloud Hosting</h3>
-						<p>Experience the benefits of flexible and reliable cloud hosting. Our experts will guide you through a seamless migration process, optimizing performance and reducing the burden of managing physical servers.						</p>
+						<h3 className='title__include'>Cloud Hosting</h3>
+						<p className='text__include'>Experience the benefits of flexible and reliable cloud hosting. Our experts will guide you through a seamless migration process, optimizing performance and reducing the burden of managing physical servers.						</p>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img  src={card5}/>
-						<h3>Mobile Development</h3>
-						<p>Reach your target audience on their preferred devices with our mobile development services. We specialize in building intuitive and feature-rich mobile applications for both iOS and Android platforms.		</p>
+						<h3 className='title__include'>Mobile Development</h3>
+						<p className='text__include'>Reach your target audience on their preferred devices with our mobile development services. We specialize in building intuitive and feature-rich mobile applications for both iOS and Android platforms.		</p>
 				</div>
 				<div className="services__branches__coontainer__boxes">
 					<img src={card6}/>
-						<h3>UX/UI</h3>
-						<p>Deliver exceptional user experiences with our UX/UI design services. Our designers combine aesthetics, usability, and functionality to create interfaces that engage users and drive customer satisfaction.		</p>
+						<h3 className='title__include'>UX/UI</h3>
+						<p className='text__include'>Deliver exceptional user experiences with our UX/UI design services. Our designers combine aesthetics, usability, and functionality to create interfaces that engage users and drive customer satisfaction.		</p>
 				</div>
 
 
