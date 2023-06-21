9fa6a57188bef30000778be802a48eea8d3db7af
SFERAS <rthyssen73@gmail.com>
2023-06-20 21:01:49 -0300

    howGlobePicture mark added

--- src/components/Routes/how/howGlobePicture/HowGlobePicture.jsx
+++ src/components/Routes/how/howGlobePicture/HowGlobePicture.jsx
@@ -5,7 +5,7 @@ function HowGlobePicture() {
 	return (
 		<div className='howGlobePicture'>
 			<div className="howGlobePicture__container">
-				<h2 className='title__include'>According to a survey by Deloitte, approximately 53% of organizations worldwide outsource some or all of their IT functions. (Source: Deloitte Global Outsourcing Survey)</h2>
+				<h2 className='title__include'>According to a survey by Deloitte, approximately <mark>53%</mark>  of organizations worldwide <mark>outsource some or all of their IT functions.</mark>  (Source: Deloitte Global Outsourcing Survey)</h2>
 			</div>
 		</div>
 	)
