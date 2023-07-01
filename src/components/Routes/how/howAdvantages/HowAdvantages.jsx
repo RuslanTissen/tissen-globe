55cd82981689892b31978c05355976f6eaa9a9d9
SFERAS <rthyssen73@gmail.com>
2023-07-01 12:54:26 -0300

    advantages adjusted

--- src/components/Routes/how/howAdvantages/HowAdvantages.jsx
+++ src/components/Routes/how/howAdvantages/HowAdvantages.jsx
@@ -10,40 +10,40 @@ import trans from '../../../Translate'
 import { Context } from '../../../Context'
 
 function HowAdvantages() {
-	const {lang, setLang} = useContext(Context)
+	const { lang, setLang } = useContext(Context)
 	return (
 		<div className='howAdvantages'>
 			<div className="howAdvantages__container">
 				<h2 className='title__picture__include'>{trans[lang].howAdvantagesTitle}</h2>
 				<div className="howAdvantages__container__boxes">
 					<img src={howAdvant1} />
-					<h3 className='title__include'>{trans[lang].howCostSavingsTitle}</h3>
-					<p className='text__include'>{trans[lang].howCostSavingsText}</p>
+					<h3>{trans[lang].howCostSavingsTitle}</h3>
+					<p>{trans[lang].howCostSavingsText}</p>
 				</div>
 				<div className="howAdvantages__container__boxes">
 					<img src={howAdvant2} />
-					<h3 className='title__include'>{trans[lang].howCoreBusinessTitle}</h3>
-					<p className='text__include'> {trans[lang].howCoreBusinessText}				</p>
+					<h3>{trans[lang].howCoreBusinessTitle}</h3>
+					<p> {trans[lang].howCoreBusinessText}				</p>
 				</div>
 				<div className="howAdvantages__container__boxes">
 					<img src={howAdvant3} />
-					<h3 className='title__include'>{trans[lang].howRiskManagementTitle}</h3>
-					<p className='text__include'>	 {trans[lang].howRiskManagementText}</p>
-				</div>
-				<div className="howAdvantages__container__boxes">
-					<img src={howAdvant4} />
-					<h3 className='title__include'>{trans[lang].howSpecializedSkillsTitle}</h3>
-					<p className='text__include'> {trans[lang].howSpecializedSkillsText}			</p>
-				</div>
-				<div className="howAdvantages__container__boxes">
-					<img src={howAdvant5} />
-					<h3 className='title__include'>{trans[lang].howScalabilityFlexibilityTitle}</h3>
-					<p className='text__include'>	{trans[lang].howScalabilityFlexibilityText}</p>
+					<h3>{trans[lang].howRiskManagementTitle}</h3>
+					<p>	 {trans[lang].howRiskManagementText}</p>
 				</div>
 				<div className="howAdvantages__container__boxes">
 					<img src={howAdvant6} />
-					<h3 className='title__include'>{trans[lang].howServiceLevelsTitle}</h3>
-					<p className='text__include'> {trans[lang].howServiceLevelsText}	</p>
+					<h3>{trans[lang].howServiceLevelsTitle}</h3>
+					<p> {trans[lang].howServiceLevelsText}	</p>
+				</div>
+				<div className="howAdvantages__container__boxes">
+					<img src={howAdvant5} />
+					<h3>{trans[lang].howScalabilityFlexibilityTitle}</h3>
+					<p>	{trans[lang].howScalabilityFlexibilityText}</p>
+				</div>
+				<div className="howAdvantages__container__boxes">
+					<img src={howAdvant4} />
+					<h3>{trans[lang].howSpecializedSkillsTitle}</h3>
+					<p> {trans[lang].howSpecializedSkillsText}			</p>
 				</div>
 			</div>
 		</div>
