7e8c7445acf52b63862b0a12c8bdc78858893edf
SFERAS <rthyssen73@gmail.com>
2023-07-19 23:15:35 -0300

    c++ changed

--- src/components/start/technologies/Technologies.jsx
+++ src/components/start/technologies/Technologies.jsx
@@ -3,12 +3,10 @@ import "./Technologies.scss"
 import { GrMysql } from "react-icons/gr"
 import { TbBrandAngular, TbBrandVue, TbBrandReactNative, TbBrandJavascript, TbBrandFigma } from "react-icons/tb"
 import { FaPython, FaAws, FaSwift } from "react-icons/fa"
-import { SiMui, SiMicrosoftazure } from "react-icons/si"
+import { SiMui, SiMicrosoftazure, SiCplusplus } from "react-icons/si"
 import trans from '../../Translate'
 import { Context } from '../../Context'
 import { NavLink } from 'react-router-dom'
-import { CgCPlusPlus } from 'react-icons/cg'
-
 
 function Technologies() {
 	const { lang, setLang } = useContext(Context)
@@ -32,7 +30,7 @@ function Technologies() {
 				<NavLink to="Figma" onClick={scrollToTheTop} className="technologies__boxes"><TbBrandFigma /></NavLink>
 				<NavLink to="MUI" onClick={scrollToTheTop} className="technologies__boxes"><SiMui /></NavLink>
 				<NavLink to="Microsoft_Azure" onClick={scrollToTheTop} className="technologies__boxes"><SiMicrosoftazure /></NavLink>
-				<NavLink to="CPlusPlus" onClick={scrollToTheTop} className="technologies__boxes"><CgCPlusPlus /></NavLink>
+				<NavLink to="CPlusPlus" onClick={scrollToTheTop} className="technologies__boxes"><SiCplusplus /></NavLink>
 				<NavLink to="Python" onClick={scrollToTheTop} className="technologies__boxes"><FaPython /></NavLink>
 				<NavLink to="MySQL" onClick={scrollToTheTop} className="technologies__boxes"><GrMysql /></NavLink>
 			</div>
