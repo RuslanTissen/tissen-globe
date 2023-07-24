0e67703b58b2dfab7f6713f6d3a7ebf9b3f0feda
SFERAS <rthyssen73@gmail.com>
2023-07-24 16:21:23 -0300

    linkedIn added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -59,7 +59,7 @@ function Footer() {
 						<a href='https://www.instagram.com/tissentech/' target='_blank'><FaInstagram /></a>
 						<a href='https://twitter.com/tissentech' target='_blank'><FaTwitter /></a>
 						<a href='https://www.facebook.com/profile.php?id=100095302290653&is_tour_completed=true' target='_blank'><FaFacebookF /></a>
-						<a><BsLinkedin /></a>
+						<a href='https://www.linkedin.com/company/tissen/about/?viewAsMember=true' target='_blank'><BsLinkedin /></a>
 					</div>
 					<div className="footer__container__section__2__email">
 						<h3>{trans[lang].footerEmail}</h3>
