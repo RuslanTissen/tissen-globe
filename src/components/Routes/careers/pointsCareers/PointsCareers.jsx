5dc58620e8afe995bd1361bffddcdb9e22c31d59
SFERAS <rthyssen73@gmail.com>
2023-08-03 22:59:29 -0300

    pointCareers class change

--- src/components/Routes/careers/pointsCareers/PointsCareers.jsx
+++ src/components/Routes/careers/pointsCareers/PointsCareers.jsx
@@ -4,7 +4,7 @@ import "./PointsCareers.scss"
 function PointsCareers(props) {
 	let { textTitle, textP } = props
 	return (
-		<div className="pointsCareers__image__container">
+		<div className="pointscCareers__image-container">
 			<h2>{textTitle}</h2>
 
 			<p>{textP}</p>
